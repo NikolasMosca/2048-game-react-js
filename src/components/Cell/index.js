1f12f430e886cf201f040cc02d808572da19f703
NikolasMosca <ciokkkolate@yahoo.it>
2019-09-13 12:32:32 +0200

    add feature move and join + add colors to cells

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -4,7 +4,7 @@ import './style.scss';
 function Cell(props) {
   const { index, value } = props;
   return (
-    <div className={'Cell' + ((value > 0) ? ` active cell-${index}` : '')}>
+    <div className={'Cell' + ((value > 0) ? ` active cell-${index} value-${value}` : '')}>
       {(value > 0) ? value : ''}
     </div>
   );
--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -5,65 +5,74 @@ import Cell from '../Cell';
 
 class Grid extends PureComponent {
     state = {
-        cells: [
-            {
-                id: 1,
-                value: 2,
-                position: 2
-            }, {
-                id: 2,
-                value: 4,
-                position: 5
-            }, {
-                id: 3,
-                value: 2,
-                position: 3
-            }, {
-                id: 4,
-                value: 2,
-                position: 7
-            }
-        ]
+        cells: [],
+        lastId: 1
     }
 
+    getLeftPositions = () => 
+        [ [0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15] ];
+
+    getUpPositions = () => 
+        [ [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15] ];
+
+    getRightPositions = () => 
+        [
+             [0, 1, 2, 3].reverse(), [4, 5, 6, 7].reverse(), 
+             [8, 9, 10, 11].reverse(), [12, 13, 14, 15].reverse() 
+        ];
+
+    getDownPositions = () => 
+        [
+            [0, 4, 8, 12].reverse(), [1, 5, 9, 13].reverse(),
+            [2, 6, 10, 14].reverse(), [3, 7, 11, 15].reverse()
+        ];
+
     componentDidMount() {
+        let { cells, lastId } = this.state;
+        cells.push(this.addRandomCell(cells, ++lastId));
+        cells.push(this.addRandomCell(cells, ++lastId));
+        this.setState({
+            cells,
+            lastId: (++lastId)
+        })
+
         document.addEventListener("keydown", event => {
+            let { cells, lastId } = this.state;
+
             switch(event.keyCode) {
                 case 37: //Arrow left
-                    this.move([
-                        [0, 1, 2, 3],
-                        [4, 5, 6, 7],
-                        [8, 9, 10, 11],
-                        [12, 13, 14, 15]
-                    ])
+                    cells = this.move( this.getLeftPositions() )     
+                    cells = this.fusionSimilarCells(cells, this.getLeftPositions())
+                    cells = this.move( this.getLeftPositions() )     
                 break;
                 case 38: //Arrow up
-                    this.move([
-                        [0, 4, 8, 12],
-                        [1, 5, 9, 13],
-                        [2, 6, 10, 14],
-                        [3, 7, 11, 15]
-                    ])
+                    cells = this.move(this.getUpPositions())
+                    cells = this.fusionSimilarCells(cells, this.getUpPositions())
+                    cells = this.move( this.getUpPositions() )    
                 break;
                 case 39: //Arrow right
-                    this.move([
-                        [0, 1, 2, 3].reverse(),
-                        [4, 5, 6, 7].reverse(),
-                        [8, 9, 10, 11].reverse(),
-                        [12, 13, 14, 15].reverse()
-                    ], true)
+                    cells = this.move(this.getRightPositions(), true)
+                    cells = this.fusionSimilarCells(cells, this.getRightPositions())
+                    cells = this.move( this.getRightPositions(), true )    
                 break;
                 case 40: //Arrow down
-                    this.move([
-                        [0, 4, 8, 12].reverse(),
-                        [1, 5, 9, 13].reverse(),
-                        [2, 6, 10, 14].reverse(),
-                        [3, 7, 11, 15].reverse()
-                    ], true)
+                    cells = this.move(this.getDownPositions(), true)
+                    cells = this.fusionSimilarCells(cells, this.getDownPositions())
+                    cells = this.move( this.getDownPositions(), true )    
                 break;
                 default: 
                 break;
-            }           
+            }
+
+            cells.push(this.addRandomCell(cells, ++lastId));
+            if(cells && cells.length > 0) {
+                console.log('cells', cells)
+                this.setState({
+                    cells,
+                    lastId: (lastId + 1)
+                })
+                this.forceUpdate();    
+            }         
         });
     }
 
@@ -73,20 +82,70 @@ class Grid extends PureComponent {
         if(reverse) cells.reverse();
 
         let focus;
+        let tempPositions = [...positions];
         cells.map((item, index) => {
-            positions.map((p, i) => (p.includes(item.position)) ? focus = i : false)
+            tempPositions.map((p, i) => (p.includes(item.position)) ? focus = i : false)
             
-            cells[index] = this.shiftCell(item, positions[focus])
-            let i = positions[focus].indexOf(cells[index].position);
-            if(i >= 0) positions[focus].splice(i, 1);
+            cells[index] = this.shiftCell(item, tempPositions[focus])
+            let i = tempPositions[focus].indexOf(cells[index].position);
+            if(i >= 0) tempPositions[focus].splice(i, 1);
             return true;
         })
         
         
         if(reverse) cells.reverse();
-        console.log('moved cells', cells)
-        this.setState({cells})
-        this.forceUpdate();
+        return cells;
+    }
+
+    fusionSimilarCells = (cells, positions) => {
+        positions.map(row => {
+            let findValues = {};
+            row.map((position, indexPosition) => {
+                
+                let deleteCellsIndex = [];
+
+                cells.map((item, index) => {
+                    if(item.position === position) {
+                        if(typeof findValues[item.value] === 'undefined') {
+                            findValues[item.value] = index;
+                        } else {
+                            //Check if the cells are joined
+                            if(
+                                cells[findValues[item.value]].position === row[indexPosition - 1]
+                            ) {
+                                cells[ findValues[item.value] ].value += item.value;
+                                delete findValues[item.value];
+                                deleteCellsIndex.push(index);
+                            }
+                        }
+                    }
+                    return true;
+                })
+
+                //Remove cells that I have fusion
+                deleteCellsIndex.map(index => cells.splice(index, 1));
+
+                return true;
+            })
+            return true;
+        })
+        return cells;
+    }
+
+    //Create new random cell
+    addRandomCell = (cells, lastId) => {
+        let avaiablePositions = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+        let blockedPositions = [];
+        cells.map(({position}) => blockedPositions.push(position))
+        avaiablePositions = avaiablePositions.filter((i) => blockedPositions.indexOf(i) < 0);
+        
+        let randPosition = avaiablePositions[Math.floor(Math.random() * avaiablePositions.length)];
+        let randValue = (Math.floor(Math.random() * 10) < 7) ? 2 : 4;
+        return {
+            id: lastId,
+            value: randValue,
+            position: randPosition
+        }
     }
 
     //Compare sort for cells's state
